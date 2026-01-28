6e356f82d15e93460b42dcc6ca90504bcb51eab4
npcarter2025 <npcarter2025@gmail.com>
2026-01-27 22:18:46 -0800

    sequencers

---
+++ tb/agents/apb_agent/apb_sequencer.svh
@@ -0,0 +1,7 @@
+`ifndef APB_SEQUENCER_SVH
+`define APB_SEQUENCER_SVH
+
+typedef uvm_sequencer #(apb_transaction) apb_sequencer;
+
+`endif
+
---
+++ tb/agents/axi_lite_agent/axi_lite_sequencer.svh
@@ -0,0 +1,6 @@
+`ifndef AXI_LITE_SEQUENCER_SVH
+`define AXI_LITE_SEQUENCER_SVH
+
+typedef uvm_sequencer #(axi_lite_transaction) axi_lite_sequencer;
+
+`endif
